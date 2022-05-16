6c5ecdc88686469f88bf0900cdb2b881e8318108
djalmajr <djalmajr@gmail.com>
2022-05-16 11:35:37 -0300

    style(filterBy): prettier

--- src/filterBy.ts
+++ src/filterBy.ts
@@ -5,5 +5,9 @@ import type { Obj } from './types';
 export const filterBy = curry((key: string, search: string, val: Obj) => {
   const query = latinize(search).toLowerCase().trim();
 
-  return query ? latinize(val[key] as string).toLowerCase().includes(query) : true;
+  return query
+    ? latinize(val[key] as string)
+        .toLowerCase()
+        .includes(query)
+    : true;
 });
