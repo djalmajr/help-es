63911a12019002fd94a4e004a541802479773e4f
djalmajr <djalmajr@gmail.com>
2023-04-17 12:11:06 -0300

    Readded commonjs build

--- bin/build.js
+++ bin/build.js
@@ -5,33 +5,37 @@ import { globPlugin } from 'esbuild-plugin-glob';
 
 const args = process.argv.slice(2);
 const isBool = (val) => /^(true|false)$/.test(val);
+const formats = { '.cjs': 'cjs', '.js': 'esm' };
 const options = args.reduce((res, arg) => {
   const [key, val] = arg.split('=');
   return { ...res, [key]: isBool(val) ? JSON.parse(val) : val };
 }, {});
 
-/** @type {import('esbuild').BuildOptions } */
-const config = {
-  ...options,
-  entryNames: '[dir]/[name]',
-  entryPoints: ['src/!(*.d).ts'],
-  bundle: true,
-  format: 'esm',
-  outbase: 'src',
-  outdir: 'lib',
-  plugins: [
-    globPlugin(),
-    {
-      name: 'add-ext',
-      setup(build) {
-        build.onResolve({ filter: /.*/ }, (args) => {
-          if (args.importer && args.path.includes('./')) {
-            return { path: args.path + '.js', external: true };
-          }
-        });
+Object.entries(formats).forEach(([ext, fmt]) => {
+  /** @type {import('esbuild').BuildOptions } */
+  const config = {
+    ...options,
+    entryNames: '[dir]/[name]',
+    entryPoints: ['src/!(*.d).ts'],
+    bundle: true,
+    format: fmt,
+    outbase: 'src',
+    outdir: 'lib',
+    outExtension: { '.js': ext },
+    plugins: [
+      globPlugin(),
+      {
+        name: 'add-ext',
+        setup(build) {
+          build.onResolve({ filter: /.*/ }, (args) => {
+            if (args.importer) {
+              return { path: args.path + ext, external: true };
+            }
+          });
+        },
       },
-    },
-  ],
-};
+    ],
+  };
 
-esbuild.build(config).catch(() => process.exit(1));
+  esbuild.build(config).catch(() => process.exit(1));
+});
