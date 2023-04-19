d9781a5e9593b838dda31c0df5dd7308fcfee0cc
djalmajr <djalmajr@gmail.com>
2023-04-19 08:52:35 -0300

    Added style functions

---
+++ src/styles.ts
@@ -0,0 +1,25 @@
+const isFrag = (n: Node) => {
+  return [Node.DOCUMENT_FRAGMENT_NODE, Node.DOCUMENT_NODE].includes(n.nodeType as never);
+};
+
+export function css(arr: string[], ...val: unknown[]) {
+  const sheet = new CSSStyleSheet();
+  sheet.replaceSync(val.reduce((r: string, v, i) => r + v + arr[i + 1], arr[0]) as string);
+  return sheet;
+}
+
+export function addStyle(
+  sheets: CSSStyleSheet | CSSStyleSheet[],
+  target: Document | ShadowRoot | HTMLElement = document,
+) {
+  const styles = (<CSSStyleSheet[]>[]).concat(sheets);
+  while (!isFrag(target)) target = <Document>(target.parentNode || document);
+  styles.forEach((sheet) => {
+    const arr = (<Document>target).adoptedStyleSheets || [];
+    if (!arr.includes(sheet)) (<Document>target).adoptedStyleSheets = [...arr, sheet];
+  });
+  return () => {
+    const arr = (<Document>target).adoptedStyleSheets;
+    arr.forEach((sheet, idx, arr) => styles.includes(sheet) && arr.splice(idx, 1));
+  };
+}
--- src/useSheet.ts
+++
@@ -1,5 +0,0 @@
-export function useSheet(sheet: CSSStyleSheet, target: Document | ShadowRoot = document) {
-  if (!target.adoptedStyleSheets.includes(sheet)) {
-    target.adoptedStyleSheets = [...target.adoptedStyleSheets, sheet];
-  }
-}
