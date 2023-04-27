5442378e3593a7f8aaeac9431073b79cd371e922
djalmajr <djalmajr@gmail.com>
2023-04-27 11:50:16 -0300

    fix(isFunction): check async case

--- src/isFunction.ts
+++ src/isFunction.ts
@@ -1,6 +1,5 @@
-import { typeOf } from './typeOf';
 import type { Fn } from './types';
 
 export function isFunction(value: unknown): value is Fn<unknown[], unknown> {
-  return typeOf(value) === 'function';
+  return typeof value === 'function';
 }
