0e2886a8125396f39073a76fac96373d4ce0b397
djalmajr <djalmajr@gmail.com>
2023-08-20 18:50:04 -0300

    fix: set

--- src/set.ts
+++ src/set.ts
@@ -1,8 +1,5 @@
 import { copy } from './copy';
 import { curry } from './curry';
-import { isFunction } from './isFunction';
-import { isString } from './isString';
-import { Obj } from './types';
 
 interface SetFn {
   (path: string | string[]): <B extends object>(value: unknown, source: B) => B;
@@ -15,19 +12,15 @@ interface SetFn {
  *
  * TODO: adicionar documentação
  */
-export const set = curry((path: string | string[], value: unknown, source: Obj<unknown>) => {
-  isString(path) && (path = path.split(/[\]\.\[]/).filter(Boolean));
-
+export const set = curry((path: any, value: any, source: any) => {
+  typeof path === 'string' && (path = path.split(/[\]\.\[]/).filter(Boolean));
   const src = copy(source);
-
   let val = src;
-
   for (let i = 0; i < path.length; i++) {
-    val = val[path[i]] =
-      i === path.length - 1
-        ? ((isFunction(value) ? value(val[path[i]]) : value) as never)
-        : copy(val[path[i]] as never);
+    // prettier-ignore
+    val = val[path[i]] = i === path.length - 1
+      ? typeof value === 'function' ? value(val[path[i]]) : value
+      : val[path[i]] ? copy(val[path[i]]) : +path[i+1] && [] || {};
   }
-
   return src;
 }) as SetFn;
